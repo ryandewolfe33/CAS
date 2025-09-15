b3dc16367aad78c826425e87b3ecb5a53ee49341
Ryan DeWolfe <ryandewolfe33@gmail.com>
2025-09-15 09:46:14 -0400

    Add output type tests

--- CAS/test/conftest.py
+++ CAS/test/conftest.py
@@ -17,3 +17,13 @@ def sbm_leiden():
     adjacency = sn.data.block_model([25]*8)
     predict = sn.clustering.Leiden().fit_predict(adjacency)
     return adjacency, predict
+
+
+@pytest.fixture(scope="session")
+def sbm_sparse():
+    adjacency = sn.data.block_model([25]*8)
+    predict = sp.lil_matrix((8, 25*8), dtype="bool")
+    for i in range(8):
+        predict[i, i*8:(i+1)*8] = True
+    predit = predict.tocsr()
+    return adjacency, predict
---
+++ CAS/test/test_cas_output_type.py
@@ -0,0 +1,42 @@
+import pytest
+import numpy as np
+import scipy.sparse as sp
+from CAS import CASPostProcesser
+
+
+def test_cas_array_only_remove(sbm_leiden):
+    adjacency, predict = sbm_leiden[0], sbm_leiden[1]
+    cpp = CASPostProcesser(only_remove=True)
+    out = cpp.fit_predict(predict, adjacency)
+    assert isinstance(out, np.ndarray)
+    assert out.ndim == 1
+    assert len(out) == len(predict)
+    assert np.min(out) >= -1
+    assert np.max(out) <= np.max(predict)
+
+
+def test_cas_array_add_and_remove(sbm_leiden):
+    adjacency, predict = sbm_leiden[0], sbm_leiden[1]
+    cpp = CASPostProcesser(only_remove=False)
+    out = cpp.fit_predict(predict, adjacency)
+    assert isinstance(out, sp.csr_matrix)
+    assert out.shape == (np.max(predict)+1, len(predict))
+    assert all(out.data)
+
+
+def test_cas_sparse_only_remove(sbm_sparse):
+    adjacency, predict = sbm_sparse[0], sbm_sparse[1]
+    cpp = CASPostProcesser(only_remove=True)
+    out = cpp.fit_predict(predict, adjacency)
+    assert isinstance(out, sp.csr_matrix)
+    assert out.shape == predict.shape
+    assert all(out.data)
+
+
+def test_cas_sparse_add_and_remove(sbm_sparse):
+    adjacency, predict = sbm_sparse[0], sbm_sparse[1]
+    cpp = CASPostProcesser(only_remove=False)
+    out = cpp.fit_predict(predict, adjacency)
+    assert isinstance(out, sp.csr_matrix)
+    assert out.shape == predict.shape
+    assert all(out.data)
